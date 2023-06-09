e7c2aaf405b4a198ff89b49fd060383cb2f33ba2
Bartek <bartlomiej.lech97@gmail.com>
2023-06-09 14:31:35 +0200

    init

---
+++ deploy/deploy-factory.ts
@@ -0,0 +1,28 @@
+import { utils, Wallet } from "zksync-web3";
+import * as ethers from "ethers";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
+
+export default async function (hre: HardhatRuntimeEnvironment) {
+  // Private key of the account used to deploy
+  const wallet = new Wallet("e1e76bdf4099bce1851c780f1725fa8f40ce0b23bcedc896a02b3ad3aae97a37");
+  const deployer = new Deployer(hre, wallet);
+  const factoryArtifact = await deployer.loadArtifact("AAFactory");
+  const aaArtifact = await deployer.loadArtifact("TwoUserMultisig");
+
+  // Getting the bytecodeHash of the account
+  const bytecodeHash = utils.hashBytecode(aaArtifact.bytecode);
+
+  const factory = await deployer.deploy(
+    factoryArtifact,
+    [bytecodeHash],
+    undefined,
+    [
+      // Since the factory requires the code of the multisig to be available,
+      // we should pass it here as well.
+      aaArtifact.bytecode,
+    ]
+  );
+
+  console.log(`AA factory address: ${factory.address}`);
+}
---
+++ hardhat.config.ts
@@ -0,0 +1,30 @@
+import { HardhatUserConfig } from "hardhat/config";
+
+import "@matterlabs/hardhat-zksync-deploy";
+import "@matterlabs/hardhat-zksync-solc";
+
+const config: HardhatUserConfig = {
+  zksolc: {
+    version: "1.3.10", // Use latest available in https://github.com/matter-labs/zksolc-bin/
+    compilerSource: "binary",
+    settings: {
+      isSystem: true,
+    },
+  },
+  defaultNetwork: "zkSyncTestnet",
+  networks: {
+    hardhat: {
+      zksync: true,
+    },
+    zkSyncTestnet: {
+      url: "https://zksync2-testnet.zksync.dev",
+      ethNetwork: "goerli", // Can also be the RPC URL of the network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
+      zksync: true,
+    },
+  },
+  solidity: {
+    version: "0.8.16",
+  },
+};
+
+export default config;
